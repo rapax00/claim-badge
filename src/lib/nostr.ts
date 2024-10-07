82c66e91a7805dc99078d05affb71a273e7774a9
Rapax <gasparpelayo44@gmail.com>
2024-10-07 18:25:59 -0300

    feat: add nostr tools event converter

--- src/lib/nostr.ts
+++ src/lib/nostr.ts
@@ -1,7 +1,10 @@
 import { BadgeDefinition } from '@/types/badge';
 import { NDKEvent, NDKTag } from '@nostr-dev-kit/ndk';
+import { Event } from 'nostr-tools';
 
-export function convertToBadgeDefinition(badge: NDKEvent): BadgeDefinition {
+export function convertNDKEventToBadgeDefinition(
+  badge: NDKEvent
+): BadgeDefinition {
   const badgeDefinition: BadgeDefinition = {
     id: badge.id,
     name: '',
@@ -13,6 +16,42 @@ export function convertToBadgeDefinition(badge: NDKEvent): BadgeDefinition {
   };
 
   const tags: NDKTag[] = badge.tags;
+
+  tags.find((tag) => {
+    // Name
+    if (tag[0] === 'name') {
+      badgeDefinition.name = tag[1];
+    } else if (tag[0] === 'description') {
+      badgeDefinition.description = tag[1];
+    }
+    // Image
+    else if (tag[0] === 'image') {
+      badgeDefinition.image = tag[1];
+
+      const size = tag[1].split('x');
+
+      badgeDefinition.width = parseInt(size[0]);
+      badgeDefinition.height = parseInt(size[1]);
+    }
+  });
+
+  return badgeDefinition;
+}
+
+export function convertNostrToolsEventToBadgeDefinition(
+  badge: Event
+): BadgeDefinition {
+  const badgeDefinition: BadgeDefinition = {
+    id: badge.id,
+    name: '',
+    description: '',
+    created_at: 0,
+    image: '',
+    width: 0,
+    height: 0,
+  };
+
+  const tags: string[][] = badge.tags;
 
   tags.find((tag) => {
     // Name
