b44f4d940de7c658207ef19ee71773c2282857f7
Rapax <gasparpelayo44@gmail.com>
2024-10-18 18:45:37 -0300

    fix: move nfc provider

--- src/app/claim/[definitionId]/page.tsx
+++ src/app/claim/[definitionId]/page.tsx
@@ -1,18 +1,15 @@
 import { ClaimForm } from '@/components/ClaimForm';
-import { InjectedNFCProvider } from '@/context/InjectedNFC';
 
-type Props = {
+interface Props {
   params: { definitionId: string };
-};
+}
 
 export default function Page(props: Props) {
   const { params } = props;
 
   return (
-    <InjectedNFCProvider>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <ClaimForm definitionId={params.definitionId} />
-      </div>
-    </InjectedNFCProvider>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <ClaimForm definitionId={params.definitionId} />
+    </div>
   );
 }
