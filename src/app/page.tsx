c9f3af142a3f4aa77ad52a9cc8451af1210a3c3c
Rapax <gasparpelayo44@gmail.com>
2024-10-18 18:45:24 -0300

    feat: add new badges, move nfc provider and update Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { BadgeCarousel } from '@/components/BadgeCarousel';
 import { requiredEnvVar } from '@/lib/utils';
 import { BadgeInfo } from '@/types/badge';
+import Home from '@/components/Home';
+import { InjectedNFCProvider } from '@/context/InjectedNFC';
 
 const publicKey = requiredEnvVar('NOSTR_PUBLIC_KEY');
 
@@ -11,12 +12,22 @@ const badgesInfo: BadgeInfo[] = [
     definitionId:
       'ab73a8fed4c7b2869b433e6e068d8316001cacd1ddea4769bb4dce1567d47eca',
   },
+  {
+    name: 'test-rock',
+    definitionId:
+      '8c107a72b82b64d045acfba156e0a22a843e40b0534efeb94efda54dabcd5c20',
+  },
+  {
+    name: 'test-drug',
+    definitionId:
+      'a2b7f323372ced597d367f19522146cf1bdca36f21540662c45884984d1a0231',
+  },
 ];
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <BadgeCarousel publicKey={publicKey} badgesInfo={badgesInfo} />
-    </div>
+    <InjectedNFCProvider>
+      <Home publicKey={publicKey} badgesInfo={badgesInfo} />
+    </InjectedNFCProvider>
   );
 }
