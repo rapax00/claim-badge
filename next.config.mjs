8014eed91283020564d845ce145fba95936b2b69
Rapax <gasparpelayo44@gmail.com>
2024-10-23 12:55:08 -0300

    fix: update next env

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_NOSTR_BADGE_EMITTER_PRIV: process.env.NOSTR_BADGE_EMITTER_PRIV,
+    NEXT_NOSTR_BADGE_EMITTER_PUB: process.env.NOSTR_BADGE_EMITTER_PUB,
+  },
   images: {
     remotePatterns: [
       {
