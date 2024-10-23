eef15b5057ed166f593bb489d245b9dbafe3bcbc
Rapax <gasparpelayo44@gmail.com>
2024-10-23 12:58:39 -0300

    feat: use auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,17 @@
-import { requiredEnvVar } from '@/lib/utils';
-import { BadgeInfo } from '@/types/badge';
-import Home from '@/components/Home';
+'use client';
+
 import { InjectedNFCProvider } from '@/context/InjectedNFC';
-
-const publicKey = requiredEnvVar('NOSTR_PUBLIC_KEY');
-
-// Simple info for search badges
-const badgesInfo: BadgeInfo[] = [
-  {
-    name: 'test-1',
-    definitionId:
-      'ab73a8fed4c7b2869b433e6e068d8316001cacd1ddea4769bb4dce1567d47eca',
-  },
-  {
-    name: 'test-rock',
-    definitionId:
-      '8c107a72b82b64d045acfba156e0a22a843e40b0534efeb94efda54dabcd5c20',
-  },
-  {
-    name: 'test-drug',
-    definitionId:
-      'a2b7f323372ced597d367f19522146cf1bdca36f21540662c45884984d1a0231',
-  },
-];
+import Login from '@/components/Login'; // Import the Login component
+import { AuthProvider } from '@/context/Auth';
 
 export default function Page() {
   return (
-    <InjectedNFCProvider>
-      <Home publicKey={publicKey} badgesInfo={badgesInfo} />
-    </InjectedNFCProvider>
+    <>
+      <InjectedNFCProvider>
+        <AuthProvider>
+          <Login />
+        </AuthProvider>
+      </InjectedNFCProvider>
+    </>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { toast } from '@/hooks/use-toast';
+import { useAuth } from '@/hooks/useAuth';
+import { QrCodeIcon } from 'lucide-react';
+import Home from './Home';
+import { BadgeInfo } from '@/types/badge';
+
+const publicKey: string = process.env.NEXT_NOSTR_BADGE_EMITTER_PUB!;
+
+// Simple info for search badges
+const badgesInfo: BadgeInfo[] = [
+  {
+    name: 'test-1',
+    definitionId:
+      'ab73a8fed4c7b2869b433e6e068d8316001cacd1ddea4769bb4dce1567d47eca',
+  },
+  {
+    name: 'test-rock',
+    definitionId:
+      '8c107a72b82b64d045acfba156e0a22a843e40b0534efeb94efda54dabcd5c20',
+  },
+  {
+    name: 'test-drug',
+    definitionId:
+      'a2b7f323372ced597d367f19522146cf1bdca36f21540662c45884984d1a0231',
+  },
+];
+
+export default function Login() {
+  const { handleLogin, isAuthenticated } = useAuth();
+  const [privateKey, setPrivateKey] = useState('');
+
+  const handleLoginClick = () => {
+    handleLogin(privateKey);
+  };
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Home publicKey={publicKey} badgesInfo={badgesInfo} />
+      ) : (
+        <Card className="w-full max-w-md mx-auto ">
+          <CardHeader>
+            <CardTitle>Admin Login</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="flex items-center space-x-2">
+              <Input
+                type="password"
+                value={privateKey}
+                onChange={(e) => setPrivateKey(e.target.value)}
+                placeholder="Enter private key"
+              />
+              <Button onClick={handleLoginClick} className="w-fit">
+                Login
+              </Button>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Button
+                onClick={() =>
+                  toast({ description: 'Not implemented yet', duration: 3000 })
+                }
+                className="w-full"
+              >
+                <QrCodeIcon className="h-4 w-4 mr-2" />
+                Scan QR Code
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      )}
+    </>
+  );
+}
