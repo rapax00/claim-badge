fc7e0c7a301e674a9d4f3f6172e38abbbed4d969
Rapax <gasparpelayo44@gmail.com>
2024-10-31 15:57:56 -0300

    fix: cors problems

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,26 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Reemplaza "*" con tu dominio si lo necesitas más restringido
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
